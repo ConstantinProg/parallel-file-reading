2d532779037b659d4435689db1f6939bb8933423
constantin <constantin.prog@gmail.com>
2024-12-24 19:27:19 +0700

    Create ParallelFileReading project

---
+++ src/ParallelFileReading/ParallelFileReader.cs
@@ -0,0 +1,22 @@
+﻿namespace ParallelFileReading;
+
+public static class ParallelFileReader
+{
+    public static async Task<int> CountSpacesInFileAsync(string filePath)
+    {
+        var content = await File.ReadAllTextAsync(filePath);
+        return content.Count(c => c == ' ');
+    }
+
+    public static async Task<int> CountSpacesInDirectoryAsync(string folderPath)
+    {
+        var files = Directory.GetFiles(folderPath);
+
+        var tasks = files.Select(file =>
+            CountSpacesInFileAsync(file)
+        );
+
+        int[] results = await Task.WhenAll(tasks);
+        return results.Sum();
+    }
+}
---
+++ src/ParallelFileReading/Program.cs
@@ -0,0 +1,31 @@
+﻿using ParallelFileReading;
+using System.Diagnostics;
+
+const string TestFilesDirectoryName = "TestFiles";
+string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), TestFilesDirectoryName);
+var fileCollection = Directory.GetFiles(directoryPath, "*.txt", SearchOption.AllDirectories);
+
+Stopwatch stopwatch = new Stopwatch();
+
+// Task 1: Read 3 files in parallel and calculate the number of spaces in them
+const int TOTAL_FILES_TO_READ = 3;
+stopwatch.Start();
+
+var tasks = new List<Task<int>>(TOTAL_FILES_TO_READ);
+foreach (var filePath  in fileCollection.Take(TOTAL_FILES_TO_READ))
+    tasks.Add(ParallelFileReader.CountSpacesInFileAsync(filePath));
+
+int[] results = await Task.WhenAll(tasks);
+int totalSpaces = results.Sum();
+stopwatch.Stop();
+
+Console.WriteLine($"Total spaces in {TOTAL_FILES_TO_READ} files: {totalSpaces}");
+Console.WriteLine($"Time taken for Task 1: {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Restart();
+
+// Task 2: Read all files in the directory and count spaces 
+int totalDirectorySpaces = await ParallelFileReader.CountSpacesInDirectoryAsync(directoryPath);
+stopwatch.Stop();
+Console.WriteLine($"Total spaces in files from folder: {totalDirectorySpaces}");
+Console.WriteLine($"Time taken for Task 2: {stopwatch.ElapsedMilliseconds} ms");
